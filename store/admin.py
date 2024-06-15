6289996f9c70d8a7ac46a9a3af0730a00b79b912
srakshitha296 <srakshitha296@gmail.com>
2024-06-16 00:26:53 +0530

    created model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import category, product, customer, order
 
-# Register your models here.
+admin.site.register(category)
+admin.site.register(customer)
+admin.site.register(product)
+admin.site.register(order)
