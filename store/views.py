d7f1d88940d49ac14672bc0f1d6a5b1952bd8e87
srakshitha296 <srakshitha296@gmail.com>
2024-06-16 23:57:22 +0530

    login and logout

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import product
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
@@ -11,8 +12,23 @@ def about(request):
     return render(request, 'about.html', {})
 
 def login_user(request):
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Login Successful!"))
+            return redirect('home')
+        else:
+            login(request, user)
+            messages.success(request, ("Error logging in"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
 
 def logout_user(request):
-    return render(request, 'logout.html', {})
+    logout(request)
+    messages.success(request, ("Logged Out"))
+    return redirect('home')
 
