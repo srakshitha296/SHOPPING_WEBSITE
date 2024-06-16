167c8faaca58271d95410339181ad062a5c36e86
srakshitha296 <srakshitha296@gmail.com>
2024-06-16 23:05:09 +0530

    login and logout

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import product
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
@@ -8,3 +9,10 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    return render(request, 'logout.html', {})
+
