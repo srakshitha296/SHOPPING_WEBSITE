0e74e3366a2a7a5843c0d82e2eb041860d5a2b70
srakshitha296 <srakshitha296@gmail.com>
2024-06-17 00:59:45 +0530

    Registration page

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 
 def home(request):
@@ -32,3 +36,22 @@ def logout_user(request):
     messages.success(request, ("Logged Out"))
     return redirect('home')
 
+def register_user(request):
+    form = SignUpForm()
+    #if the user if grtting registered
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #log in user
+            user = authenticate(username = username, password=password)
+            login(request, user)
+            messages.success(request, ("Registered Successfully!"))
+            return redirect('home')
+        else:
+            messages.success(request, " There was a problrm registering...Please try again.")
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
