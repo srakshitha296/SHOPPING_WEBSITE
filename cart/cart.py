8867d35ed3024f6c2c6e2b3ec7e69c4c33620d79
srakshitha296 <srakshitha296@gmail.com>
2024-06-30 13:30:51 +0530

    added product to cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,3 @@
-
-
 class Cart():
 	def __init__(self, request):
 		self.session = request.session
@@ -13,3 +11,13 @@ class Cart():
 
 
 		self.cart = cart
+
+	def add(self, product):
+		product_id = str(product.id)
+
+		if product_id in self.cart:
+			pass
+		else:
+			self.cart[product_id] = {'price' : str(product.price)}
+			
+		self.session.modified = True
